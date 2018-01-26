822c249f276a4daa3695827efd77f0dc0b7f606b
Shirley Cohen <shirley.cohen@rackspace.com>
2018-01-26 09:50:56 -0600

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,19 +1,19 @@
 drop table if exists Category;
 
-\i C:/utcs_work/cs327e_spring_2018/sql_scripts/inclass_lab2/create_category.sql
+\i C:/utcs_work/cs327e_spring_2018/sql_scripts/create_category.sql
 
 drop table if exists Product;
 
-\i C:/utcs_work/cs327e_spring_2018/sql_scripts/inclass_lab2/create_product.sql
+\i C:/utcs_work/cs327e_spring_2018/sql_scripts/create_product.sql
 
 drop table if exists Product_Store;
 
-\i C:/utcs_work/cs327e_spring_2018/sql_scripts/inclass_lab2/create_product_store.sql
+\i C:/utcs_work/cs327e_spring_2018/sql_scripts/create_product_store.sql
 
 drop table if exists Store;
 
-\i C:/utcs_work/cs327e_spring_2018/sql_scripts/inclass_lab2/create_store.sql
+\i C:/utcs_work/cs327e_spring_2018/sql_scripts/create_store.sql
 
 drop table if exists Subcategory;
 
-\i C:/utcs_work/cs327e_spring_2018/sql_scripts/inclass_lab2/create_subcategory.sql
+\i C:/utcs_work/cs327e_spring_2018/sql_scripts/create_subcategory.sql
