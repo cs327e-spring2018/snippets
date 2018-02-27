2407381ff9ab8b3e32ba35c047e7a69ae4e5aef3
Shirley Cohen <shirley.cohen@rackspace.com>
2018-02-27 11:21:56 -0600

    example indexes on tickit database

--- create_indexes.sql
+++ create_indexes.sql
@@ -7,16 +7,17 @@ create index listing_numtickets_idx on Listing(numtickets);
 /* Index to speed-up the following query:
 select * from listing where numtickets > 10 and priceperticket between 50 and 100; 
 */
-create index listing_numtickets_priceperticket on listing(numtickets, priceperticket);
+create index listing_numtickets_priceperticket_idx on Listing(numtickets, priceperticket);
 
-/* Index to speed-up the following query:
+/* Indexes to speed-up the following query:
 select l.*, s.pricepaid, s.commission 
-from listing l join sales s
+from Listing l join Sales s
 on l.listid = s.listid
 where numtickets > 10 and priceperticket between 50 and 100
 and commission > 100; 
 */
-create index sales_commission_idx on sales(commission);
+create index listing_numtickets_priceperticket_listid_idx on Listing(numtickets, priceperticket, listid);
+create index sales_commission_listid_idx on Sales(commission, listid);
 
 /* Indexes to speed-up the following query:
 select s.sellerid, u.username, u.email, sum(qtysold)
@@ -26,7 +27,6 @@ group by s.sellerid, u.username, u.email
 order by sum(qtysold) desc; 
 */
 create index sales_sellerid_qtysold_idx on Sales(sellerid, qtysold);
-
 create index users_city_userid_username_email_idx on Users(city, userid, username, email)
      where city = 'Houston';
 
